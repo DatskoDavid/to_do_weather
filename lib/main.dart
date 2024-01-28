39d4f38f3b38a1510f95f2ddf023974df2489ede
Datsko David <datskodavid@gmail.com>
2024-01-29 00:33:25 +0200

    some refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_weather/data/data_sources/remote_data_source.dart';
 import 'package:to_do_weather/domain/di/injector.dart';
+import 'package:to_do_weather/presentation/screens/bottom_nav_bar_controller.dart';
 
 import 'data/data_sources/database_data_source.dart';
 import 'presentation/screens/tasks_screen.dart';
@@ -8,6 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseDataSource.initializeHive();
   initInjector();
+
   runApp(const MyApp());
 }
 
@@ -17,12 +20,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To do / Weather',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TasksScreen(),
+      home: const BottomNavBarController(),
     );
   }
 }
